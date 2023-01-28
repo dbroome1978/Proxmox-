8c8979c5e069755fc4e1ac60a860ed7e08f8b85b
dbroome1978 <dbroome@gmail.com>
2023-01-27 22:30:20 -0600

    Update premiumizer-install.sh

--- setup/premiumizer-install.sh
+++ setup/premiumizer-install.sh
@@ -59,7 +59,7 @@ $STD apt-get install -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
 $STD apt-get install -y python3
-$STD apt-get install -y git-core
+$STD apt-get install -y git
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Premiumizer"
